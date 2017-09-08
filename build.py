2d184eda6ed14027c012f1ac0ff2f2d6ce8941a4
Jerry W <jerrywiltse@gmail.com>
2017-09-07 20:57:37 -0400

    remove extra args from build.py

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager, os
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args="--build missing", username=os.getenv("CONAN_USERNAME"), channel=os.getenv("CONAN_CHANNEL"))
+    builder = ConanMultiPackager(args="--build missing")
     builder.add_common_builds()
     builder.run()
