e7d75a5efa0bccb4b472594bf61b346c8c22de26
Jerry W <jerrywiltse@gmail.com>
2017-09-11 22:14:55 -0400

    move remotes to build.py

--- build.py
+++ build.py
@@ -2,6 +2,8 @@ from conan.packager import ConanMultiPackager, os
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args="--build missing")
+    builder = ConanMultiPackager(
+        upload="https://api.bintray.com/conan/bincrafters/public-conan",
+        remotes="https://api.bintray.com/conan/conan-community/conan")
     builder.add_common_builds()
     builder.run()
