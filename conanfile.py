1e558508cad8212fe26421b65214cbf9233e741c
Jerry W <jerrywiltse@gmail.com>
2017-09-11 21:14:45 -0400

    new branch naming strategy for git-branch

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools, os
 
 class LibtinsConan(ConanFile):
     name = "libtins"
-    version = "3.5"
+    version = "3.6"
     author = "mfontanini"
     description = "High-level, multiplatform C++ network packet sniffing and crafting library"
     license = "https://github.com/mfontanini/libtins/blob/master/LICENSE"
@@ -16,32 +16,32 @@ class LibtinsConan(ConanFile):
         "enable_wpa2": [True, False],
         "enable_tcpip": [True, False],
         "enable_ack_tracker": [True, False],
-        "win_capture_lib": ["winpcap", "npcap"],
+        "winpcap_mode": [True, False],
         "enable_tcp_stream_custom_data": [True, False]
     }
-    default_options = "shared=True", "enable_pcap=True", "enable_cxx11=True", "enable_dot11=True", "enable_wpa2=True", "enable_tcpip=True", "enable_ack_tracker=True", "enable_tcp_stream_custom_data=True", "win_capture_lib=npcap"
+    default_options = "shared=True", "enable_pcap=True", "enable_cxx11=True", "enable_dot11=True", "enable_wpa2=True", "enable_tcpip=True", "enable_ack_tracker=True", "enable_tcp_stream_custom_data=True", "winpcap_mode=False"
     generators = "cmake"
 
     def requirements(self):
         if self.options.enable_pcap:
             if self.settings.os == "Windows":
-                if self.options.win_capture_lib == "npcap":
+                if self.options.winpcap_mode:
                     self.requires.add("WinPcap/4.1.2@RoliSoft/stable")
                 else:
                     self.requires.add("npcap/0.93@bincrafters/testing")
+                    self.options["npcap"].winpcap_mode = self.options.winpcap_mode
             else:
                 self.requires.add("libpcap/1.8.1@uilianries/stable")
         if self.options.enable_wpa2:
             self.requires.add("OpenSSL/1.0.2l@conan/stable")
         if self.options.enable_ack_tracker or self.options.enable_tcp_stream_custom_data:
-            self.requires.add("Boost/1.64.0@conan/stable")
-
+            self.requires.add("Boost.Icl/1.64.0@bincrafters/testing")
+            self.requires.add("Boost.Any/1.64.0@bincrafters/testing")
+            
     def source(self):
         source_url =  "https://github.com/mfontanini/libtins"
-        archive_name = "v" + self.version        
-        tools.get("{0}/archive/{1}.tar.gz".format(source_url, archive_name))
-        os.rename(self.name + "-" + self.version  , self.name)
-
+        self.run("git clone --depth=1 --branch=master {0}.git".format(source_url)) 
+   
     def build(self):
         conan_magic_lines = """PROJECT(libtins)
     INCLUDE(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
